d4ac2cfef477c6123139f8c6e67b91facb395149
Emmanuel Pastor <pastorenuel@gmail.com>
2024-08-22 19:42:01 +0200

    initial commit of lifetimes

--- src/main.rs
+++ src/main.rs
@@ -27,5 +27,28 @@ fn main() {
     // println!("User age: {}", user_age_2.unwrap());
     // println!("User age: {}",UserAge::from(5));
     // println!("User age: {}", user_age_3);
-    crate::tdd::run();
+    // crate::tdd::run();
+    let a = 14;
+    let mut b = 6;
+    compute(&a,&mut b);
+    let mut c = String::new();
+    println!("y: {}", as_str(&a, &mut c));
+}
+
+fn compute(input: &u32, output: &mut u32) {
+    let mut temp = *output;
+    if *input > 10 {
+        temp = 1;
+    }
+    if *input > 5 {
+        temp *= 2;
+    }
+    *output = temp;
+    println!("Input: {}, Output: {}", input, output);
+}
+
+/// Lifetimes
+fn as_str<'a>(x: &'a u32, y: &'a mut String) -> &'a str {
+    *y = format!("{}", x);
+    y.as_str()
 }
