5a4450485e37c2b4efe93fd75c01c39a43b18a39
Emmanuel Pastor <pastorenuel@gmail.com>
2024-08-22 02:02:22 +0200

    working with types

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
+#![allow(unused_imports)]
+
 mod collectionz;
 mod io;
 mod taxonomy;
+mod dt;
+mod traits;
+mod tdd;
 
 use collectionz::using_vec::run;
 use collectionz::using_hashmap::run_hashmap;
 use collectionz::using_strings::run_strings;
 use taxonomy::plant::litter::{grow, PlantClassification, Creeper, PlantInfo};
+use dt::date::{get_native_date, working_with_mutex};
+use crate::traits::UserAge;
 
 fn main() {
     // run();
     // run_hashmap();
     // run_strings();
-    let basil = PlantClassification::Creeper(Creeper {
-        info: PlantInfo {
-            botanica_name: String::from("Basil"),
-            age: 1,
-            is_grown: true,
-            species: String::from("Creeper"),
-            family: String::from("Creeper"),
-            description: "This is a simple description for a creeper".to_string(),
-        }
-    });
-    println!("{:?}", basil);
-    grow(Box::new(basil));
-
+    // get_native_date();
+    // working_with_mutex();
+    // let user_age = crate::traits::try_from(10);
+    // let user_age_2 = crate::traits::try_into(4);
+    // let user_age_3: UserAge = 6.into();
+    // println!("User age: {}", user_age.unwrap());
+    // println!("User age: {}", user_age_2.unwrap());
+    // println!("User age: {}",UserAge::from(5));
+    // println!("User age: {}", user_age_3);
+    crate::tdd::run();
 }
